c54069628c33be402d33e1fcf2e8df845b8766e7
Vedant Singh <88138339+danticu@users.noreply.github.com>
2024-11-15 10:19:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (54.96%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,5 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        /*
         String result="";
         int minLength=Integer.MAX_VALUE;
         for(String s: strs)
@@ -20,5 +21,42 @@ class Solution {
             result=result+c;
         }
         return result;
+
+
+        */
+        int minLength=Integer.MAX_VALUE;
+        for(String s: strs)
+        {
+            minLength=Math.min(minLength, s.length());
+        }
+        int left=0;
+        int right=minLength;
+        while(left<=right)
+        {
+            int mid=(right+left)/2;
+            if(checkPrefix(strs, mid))
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+            }
+        }
+        int maxLength=(left+right)/2;
+        return strs[0].substring(0, maxLength);
+
+    }
+    private boolean checkPrefix(String strs[], int length)
+    {
+        String s=strs[0].substring(0, length);
+        for(int i=1; i<strs.length; i++)
+        {
+            if(!strs[i].startsWith(s))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
