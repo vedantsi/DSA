0833e443616f408ba775b42130b7e4286e0209cc
Vedant Singh <88138339+vedantsi@users.noreply.github.com>
2025-04-09 11:18:17 +0530

    Time: 4 ms (29.93%) | Memory: 44.6 MB (41.12%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,33 +1,27 @@
 class Solution {
+    private boolean checkDistinct(int nums[], int i)
+    {
+        HashSet<Integer> set=new HashSet<>();
+        for(int j=i; j<nums.length; j++)
+        {
+            if(set.contains(nums[j]))
+            {
+                return false;
+            }
+            set.add(nums[j]);
+        }
+        return true;
+    }
     public int minimumOperations(int[] nums) {
         int result=0;
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int val: nums)
+        for(int i=0; i<nums.length; i+=3)
         {
-            list.add(val);
-        }
-        while(true)
-        {
-            HashSet<Integer> set=new HashSet<>();
-            boolean hasDuplicate=false;
-            for (int val : list) {
-                if (set.contains(val)) {
-                    hasDuplicate = true;
-                    break;
-                }
-                set.add(val);
-            }
-
-            if (!hasDuplicate) {
-                break;
-            }
-            int toRemove = Math.min(3, list.size());
-            for (int i = 0; i < toRemove; i++) {
-                list.remove(0);
+            if(checkDistinct(nums, i))
+            {
+                return result;
             }
             result++;
         }
-
         return result;
     }
 } 
