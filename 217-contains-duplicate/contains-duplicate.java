3a03bc3bef05a9a8ee45093d115487e44a124756
Vedant Singh <88138339+vedantsi@users.noreply.github.com>
2025-04-16 06:03:54 +0530

    Time: 13 ms (42.08%) | Memory: 57.9 MB (59.47%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        /*
         Arrays.sort(nums);
         for(int i=0; i<nums.length-1; i++)
         {
@@ -9,5 +10,19 @@ class Solution {
             }
         }
         return false;
+
+    }//Time complexity: O(N log N)+ O(N)
+    */
+        HashSet<Integer> checkDuplicate=new HashSet<Integer>();
+        for(int n:nums)
+        {
+            if(checkDuplicate.contains(n))
+            {
+                return true;
+            }
+            checkDuplicate.add(n);
+        }
+        return false;
     }
+
 }
