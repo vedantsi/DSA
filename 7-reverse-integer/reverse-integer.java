bde195893aa4708a12e18cbce8b9fee9a8d84890
Vedant Singh <88138339+vedantsi@users.noreply.github.com>
2025-04-19 01:43:13 +0530

    Time: 1 ms (83.36%) | Memory: 41.2 MB (34.15%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -97,7 +97,7 @@ class Solution {
 
 
 
-        */
+        
 
 
 
@@ -117,5 +117,40 @@ class Solution {
             return sign*(int)reverse;
         }
         return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        */
+        int reverse=0;
+        while(x!=0)
+        {
+            int digit=x%10;
+            if(reverse > Integer.MAX_VALUE/10 || ((reverse == Integer.MAX_VALUE/10)&& digit>7))
+            {
+                return 0;
+            }
+            if(reverse < Integer.MIN_VALUE/10 || ((reverse == Integer.MIN_VALUE/10)&& digit<-8))
+            {
+                return 0;
+            }
+            reverse=reverse*10+digit;
+            x=x/10;
+        }
+        return reverse;
     }
 }
